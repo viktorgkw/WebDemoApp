3c96f6c58b2d409165f5aaa89f9b1521e6987762
Viktor Georgakiev <ciganskigeigerwiksa26g@gmail.com>
2022-12-07 20:57:10 +0200

    Add project files.

---
+++ WebDemoApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebDemoApp.Models;
+
+namespace WebDemoApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogViewModel = new DogViewModel();
+
+            return View(dogViewModel);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WebDemoApp/Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebDemoApp.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
